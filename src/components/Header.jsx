0834cf5e1d90d68aca7e16b9aada50cd6add55e1
sagarsharma5 <sagar.sharma.5235@gmail.com>
2024-07-28 22:23:03 +0530

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ const Header = () => {
   return (
     <header className="text-white  border-b-[1px] border-black bg-darkblue">
       <nav className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center ml-24">
+        <div className="flex items-center ml-36">
           <img
             src="\finalchristlogo.png"
             alt=""
             className="h-[110px] w-60 cursor-pointer"
           />
         </div>
-        <div className="mr-28">
+        <div className="mr-36">
           <SearchBar />
         </div>
       </nav>
